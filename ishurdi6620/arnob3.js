e70c2f081a3b06d60735185a943a5b06f4f36187
Arnob Karmokar <igeniusarnob@gmail.com>
2020-05-13 05:35:00 +0600

    Update and rename arnob2.js to arnob3.js

---
+++ ishurdi6620/arnob3.js
@@ -0,0 +1,396 @@
+async function sleep() {
+    await new Promise(r => setTimeout(r, 3000));
+}
+sleep();
+document.getElementById('counter').innerHTML = 8;
+var answerId;
+switch (question_id) {
+    case 5:
+        answerId = 17;
+        break;
+    case 6:
+        answerId = 22;
+        break;
+    case 7:
+        answerId = 25;
+        break;
+    case 8:
+        answerId = 29;
+        break;
+    case 9:
+        answerId = 33;
+        break;
+    case 10:
+        answerId = 38;
+        break;
+    case 11:
+        answerId = 41;
+        break;
+    case 12:
+        answerId = 46;
+        break;
+    case 13:
+        answerId = 49;
+        break;
+    case 14:
+        answerId = 54;
+        break;
+    case 15:
+        answerId = 59;
+        break;
+    case 16:
+        answerId = 62;
+        break;
+    case 17:
+        answerId = 67;
+        break;
+    case 18:
+        answerId = 69;
+        break;
+    case 19:
+        answerId = 74;
+        break;
+    case 20:
+        answerId = 78;
+        break;
+    case 21:
+        answerId = 81;
+        break;
+    case 22:
+        answerId = 88;
+        break;
+    case 23:
+        answerId = 89;
+        break;
+    case 24:
+        answerId = 94;
+        break;
+    case 25:
+        answerId = 99;
+        break;
+    case 26:
+        answerId = 101;
+        break;
+    case 27:
+        answerId = 106;
+        break;
+    case 28:
+        answerId = 112;
+        break;
+    case 29:
+        answerId = 113;
+        break;
+    case 30:
+        answerId = 119;
+        break;
+    case 31:
+        answerId = 123;
+        break;
+    case 32:
+        answerId = 128;
+        break;
+    case 33:
+        answerId = 129;
+        break;
+    case 34:
+        answerId = 133;
+        break;
+    case 35:
+        answerId = 138;
+        break;
+    case 36:
+        answerId = 141;
+        break;
+    case 37:
+        answerId = 147;
+        break;
+    case 38:
+        answerId = 149;
+        break;
+    case 39:
+        answerId = 155;
+        break;
+    case 40:
+        answerId = 157;
+        break;
+    case 41:
+        answerId = 161;
+        break;
+    case 42:
+        answerId = 167;
+        break;
+    case 43:
+        answerId = 169;
+        break;
+    case 44:
+        answerId = 173;
+        break;
+    case 45:
+        answerId = 178;
+        break;
+    case 46:
+        answerId = 183;
+        break;
+    case 47:
+        answerId = 186;
+        break;
+    case 48:
+        answerId = 189;
+        break;
+    case 49:
+        answerId = 193;
+        break;
+    case 50:
+        answerId = 197;
+        break;
+    case 51:
+        answerId = 201;
+        break;
+    case 52:
+        answerId = 207;
+        break;
+    case 53:
+        answerId = 211;
+        break;
+    case 54:
+        answerId = 213;
+        break;
+    case 55:
+        answerId = 220;
+        break;
+    case 57:
+        answerId = 225;
+        break;
+    case 58:
+        answerId = 232;
+        break;
+    case 59:
+        answerId = 235;
+        break;
+    case 60:
+        answerId = 239;
+        break;
+    case 61:
+        answerId = 243;
+        break;
+    case 62:
+        answerId = 245;
+        break;
+    case 63:
+        answerId = 249;
+        break;
+    case 64:
+        answerId = 253;
+        break;
+    case 65:
+        answerId = 257;
+        break;
+    case 66:
+        answerId = 262;
+        break;
+    case 67:
+        answerId = 265;
+        break;
+    case 68:
+        answerId = 271;
+        break;
+    case 69:
+        answerId = 275;
+        break;
+    case 70:
+        answerId = 279;
+        break;
+    case 71:
+        answerId = 282;
+        break;
+    case 72:
+        answerId = 286;
+        break;
+    case 73:
+        answerId = 291;
+        break;
+    case 74:
+        answerId = 293;
+        break;
+    case 75:
+        answerId = 299;
+        break;
+    case 76:
+        answerId = 303;
+        break;
+    case 77:
+        answerId = 307;
+        break;
+    case 78:
+        answerId = 309;
+        break;
+    case 79:
+        answerId = 315;
+        break;
+    case 80:
+        answerId = 317;
+        break;
+    case 81:
+        answerId = 323;
+        break;
+    case 82:
+        answerId = 327;
+        break;
+    case 83:
+        answerId = 330;
+        break;
+    case 84:
+        answerId = 335;
+        break;
+    case 85:
+        answerId = 339;
+        break;
+    case 86:
+        answerId = 343;
+        break;
+    case 87:
+        answerId = 345;
+        break;
+    case 88:
+        answerId = 352;
+        break;
+    case 89:
+        answerId = 353;
+        break;
+    case 90:
+        answerId = 357;
+        break;
+    case 91:
+        answerId = 363;
+        break;
+    case 92:
+        answerId = 365;
+        break;
+    case 93:
+        answerId = 369;
+        break;
+    case 94:
+        answerId = 376;
+        break;
+    case 95:
+        answerId = 377;
+        break;
+    case 96:
+        answerId = 381;
+        break;
+    case 97:
+        answerId = 387;
+        break;
+    case 98:
+        answerId = 389;
+        break;
+    case 99:
+        answerId = 395;
+        break;
+    case 100:
+        answerId = 398;
+        break;
+    case 101:
+        answerId = 403;
+        break;
+    case 102:
+        answerId = 405;
+        break;
+    case 103:
+        answerId = 410;
+        break;
+    case 104:
+        answerId = 414;
+        break;
+    case 105:
+        answerId = 420;
+        break;
+    case 106:
+        answerId = 421;
+        break;
+    case 107:
+        answerId = 425;
+        break;
+    case 108:
+        answerId = 431;
+        break;
+    case 109:
+        answerId = 435;
+        break;
+    case 110:
+        answerId = 437;
+        break;
+    case 111:
+        answerId = 442;
+        break;
+    case 112:
+        answerId = 448;
+        break;
+    case 113:
+        answerId = 449;
+        break;
+    case 114:
+        answerId = 453;
+        break;
+    case 115:
+        answerId = 457;
+        break;
+    case 116:
+        answerId = 461;
+        break;
+    case 117:
+        answerId = 468;
+        break;
+    case 118:
+        answerId = 470;
+        break;
+    case 119:
+        answerId = 476;
+        break;
+    case 120:
+        answerId = 479;
+        break;
+    case 121:
+        answerId = 483;
+        break;
+    case 122:
+        answerId = 485;
+        break;
+    case 123:
+        answerId = 491;
+        break;
+    case 124:
+        answerId = 495;
+        break;
+    case 125:
+        answerId = 497;
+        break;
+    case 126:
+        answerId = 502;
+        break;
+    case 127:
+        answerId = 507;
+        break;
+    case 128:
+        answerId = 509;
+        break;
+    case 129:
+        answerId = 515;
+        break;
+    case 130:
+        answerId = 517;
+        break;
+    case 131:
+        answerId = 522;
+        break;
+    case 132:
+        answerId = 527;
+        break;
+    case 133:
+        answerId = 529;
+        break;
+    default:
+        answerId = null;
+}
+
+checkAnswer(answerId);
